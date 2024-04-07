6d7ad2ea216fdb3bfe92840731f903b1950671d9
gaazoo <gaazoo@yandex.ru>
2024-04-07 15:30:27 +0700

    Готово

--- SQL_HomeWork_6/Task1.sql
+++ SQL_HomeWork_6/Task1.sql
@@ -11,17 +11,17 @@ CREATE FUNCTION format_seconds (seconds INT)
 RETURNS VARCHAR(100)
 DETERMINISTIC
 BEGIN
-    DECLARE days INT;
-    DECLARE hours INT;
-    DECLARE minutes INT;
-    DECLARE remaining_seconds INT;
-    SET days = seconds DIV (60 * 60 * 24);
+    DECLARE days INT; -- Дни
+    DECLARE hours INT; -- Часы
+    DECLARE minutes INT; -- Минуты
+    DECLARE remaining_seconds INT; -- Секунды
+    SET days = seconds DIV (60 * 60 * 24); -- Получаем кол-во дней
     SET remaining_seconds = seconds MOD (60 * 60 * 24);
-    SET hours = remaining_seconds DIV (60 * 60);
+    SET hours = remaining_seconds DIV (60 * 60); -- Получаем кол-во часов
     SET remaining_seconds = remaining_seconds MOD (60 * 60);
-    SET minutes = remaining_seconds DIV 60;
+    SET minutes = remaining_seconds DIV 60; -- Получаем кол-во минут
     SET remaining_seconds = remaining_seconds MOD 60;
-    RETURN CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', remaining_seconds, ' seconds');
+    RETURN CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', remaining_seconds, ' seconds'); -- Формируем выходные данные
 END$$
 
 DELIMITER ;
--- SQL_HomeWork_6/Task2.sql
+++ SQL_HomeWork_6/Task2.sql
@@ -6,16 +6,16 @@ CREATE DATABASE chislo;
 USE chislo;
 
 DELIMITER //
-CREATE PROCEDURE get_even(`start` INT, `end` INT)
+CREATE PROCEDURE get_even(`start` INT, `end` INT) -- Задаем диапазон чисел (от start до end)
 BEGIN
 	DECLARE i INT DEFAULT `start`;
     DECLARE res_str TEXT DEFAULT NULL;
-    WHILE  i<=`end` DO
-        IF i%2 = 0 THEN
+    WHILE  i<=`end` DO -- начинаем проверять числа на четность
+        IF i%2 = 0 THEN -- проверяем текущее число
 			IF res_str IS NULL THEN
 				SET res_str = concat(i);
 			ELSE
-				SET res_str = concat(res_str, ', ', i);
+				SET res_str = concat(res_str, ', ', i); -- добавляем четное число в выборку
 			END IF;
 		END IF;
         SET i = i + 1;
@@ -24,4 +24,4 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL get_even(1, 10);
+CALL get_even(1, 10); -- диапазон чисел для проверки
